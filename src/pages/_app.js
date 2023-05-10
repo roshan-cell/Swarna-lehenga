d08377afe9c4c388e067338efbf6395015488310
roshan-cell <95081242+roshan-cell@users.noreply.github.com>
2023-05-10 21:41:16 +0530

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import '@/styles/globals.css'
+import Footer from '../../components/Footer'
+import NavBar from '../../components/Navbar'
+
+export default function App({ Component, pageProps }) {
+  return <> <NavBar/> <Component {...pageProps} />  <Footer/> </>
+}
