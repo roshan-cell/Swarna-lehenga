b65df5cd0c559b0c6faf74e3d666db2e1f18247a
roshan-cell <95081242+roshan-cell@users.noreply.github.com>
2023-05-13 02:00:09 +0530

    Designing pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,30 +1,103 @@
-import React from "react";
+import React, { useRef } from "react";
 import Link from "next/link";
-import Image from 'next/image';
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import Image from "next/image";
+import {
+  AiOutlineShoppingCart,
+  AiFillCloseCircle,
+  AiOutlinePlusCircle,
+  AiOutlineMinusCircle,
+} from "react-icons/ai";
+import { BsFillBagCheckFill } from "react-icons/bs";
 
 const NavBar = () => {
+  const toggleCart = () => {
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else if (!ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
+    }
+  };
+
+  const ref = useRef();
+
   return (
-
-      <div className="flex flex-col md:flex-row md:justify-start justify-center items-center  py-2 shadow-xl">
-        <div className="logo mx-5">
-          <Image src="/shoeKart.png" width={200} height={30} alt="shoeKart" />
-        </div>
-
-        <div className="nav">
-          <ul className = "flex items-center space-x-6 font-bold md:text-md">
-            <Link href= {'/sneaker'}> <li>Sneaker</li> </Link>
-            <Link href= {'/formals'}> <li>Formals</li> </Link>
-            <Link href= {'/sports'}> <li>Sports</li> </Link>
-          </ul>
-        </div>
-
-        <div className="cart absolute right-0 top-4 mx-5">
-          <AiOutlineShoppingCart  className="text-xl  md:text-3xl" />
-        </div>
-
+    <div className="flex flex-col md:flex-row md:justify-start justify-center items-center  py-2 shadow-md">
+      <div className="logo mx-5">
+        <Link href={"/"}>
+          {" "}
+          <Image
+            src="/shoeKart.png"
+            width={80}
+            height={30}
+            alt="shoeKart"
+          />{" "}
+        </Link>
       </div>
 
+      <div className="nav">
+        <ul className="flex items-center space-x-6 font-bold md:text-md">
+          <Link href={"/sneaker"}>
+            {" "}
+            <li>Sneaker</li>{" "}
+          </Link>
+          <Link href={"/formals"}>
+            {" "}
+            <li>Formals</li>{" "}
+          </Link>
+          <Link href={"/sports"}>
+            {" "}
+            <li>Sports</li>{" "}
+          </Link>
+        </ul>
+      </div>
+
+      <div
+        onClick={toggleCart}
+        className="cart cursor-pointer absolute right-0 top-4 mx-5"
+      >
+        <AiOutlineShoppingCart className="text-xl  md:text-3xl my-3 " />
+      </div>
+
+      <div
+        ref={ref}
+        className="w-72 h-full sidebar cursor-pointer absolute top-0 right-0 bg-yellow-100 px-5 py-10 transform transition-transform translate-x-full"
+      >
+        <h2 className="font-bold text-xl text-center"> Shopping Cart</h2>
+        <span
+          onClick={toggleCart}
+          className="absolute top-5 right-2 cursor-pointer text-2xl text-blue-50-100"
+        >
+          {" "}
+          <AiFillCloseCircle />{" "}
+        </span>
+        <ol className="list-decimal font-semibold">
+          <li>
+            <div className="item flex my-5">
+              <span className="w-2/3 font-semibold">Sneakers</span>
+              <span className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                {" "}
+                <AiOutlinePlusCircle className="cursor-pointer bg-pink-500" />
+                <span className="mx-2"> 2 </span>{" "}
+                <AiOutlineMinusCircle className="cursor-pointer bg-pink-500" />{" "}
+              </span>
+            </div>
+          </li>
+        </ol>
+      
+        <div className="flex">
+          <button className="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+            <BsFillBagCheckFill className="m-1" /> Checkout
+          </button>
+
+          <button className="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+            <BsFillBagCheckFill className="m-1" /> Clear
+          </button>
+
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/pages/formals.js
+++ src/pages/formals.js
@@ -1,9 +1,193 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Formals = () => {
   return (
-    <div>Formals</div>
-  )
-}
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex  flex-wrap -m-4">
+            <Link href={"/product/wear-with-pride"}>
+              {" "}
+              <div className="lg:w-1/1 md:w-1/2 p-4 w-full  cursor-pointer  shodow-lg m-12">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh] block"
+                    src="https://source.unsplash.com/random/900×700/?formal-shoes"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                </div>
+              </div>
+            </Link>
 
-export default Formals
+            <Link href={"/product/wear-with-pride "}>
+              {" "}
+              <div className="lg:w-1/1 md:w-1/2 p-4 w-full  cursor-pointer  shodow-lg m-12">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh] block"
+                    src="https://source.unsplash.com/random/900×700/?formal-shoes"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Shooting Stars
+                  </h2>
+                  <p className="mt-1">$21.15</p>
+                </div>
+              </div>{" "}
+            </Link>
+
+            <Link href={"/product/wear-with-pride "}>
+              {" "}
+              <div className="lg:w-1/1 md:w-1/2 p-4 w-full  cursor-pointer  shodow-lg m-12">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh] block"
+                    src="https://source.unsplash.com/random/900×700/?formal-shoes"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Neptune
+                  </h2>
+                  <p className="mt-1">$12.00</p>
+                </div>
+              </div>{" "}
+            </Link>
+
+            <Link href={"/product/wear-with-pride "}>
+              {" "}
+              <div className="lg:w-1/1 md:w-1/2 p-4 w-full  cursor-pointer  shodow-lg m-12">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh] block"
+                    src="https://source.unsplash.com/random/900×700/?formal-shoes"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The 400 Blows
+                  </h2>
+                  <p className="mt-1">$18.40</p>
+                </div>
+              </div>{" "}
+            </Link>
+
+            <Link href={"/product/wear-with-pride "}>
+              {" "}
+              <div className="lg:w-1/1 md:w-1/2 p-4 w-full   cursor-pointer  shodow-lg m-12">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh] block"
+                    src="https://source.unsplash.com/random/900×700/?formal-shoes"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">$16.00</p>
+                </div>
+              </div>{" "}
+            </Link>
+
+            <Link href={"/product/wear-with-pride "}>
+              {" "}
+              <div className="lg:w-1/1 md:w-1/2 p-4 w-full  cursor-pointer  shodow-lg m-12">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh] block"
+                    src="https://source.unsplash.com/random/900×700/?formal-shoes"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Shooting Stars
+                  </h2>
+                  <p className="mt-1">$21.15</p>
+                </div>
+              </div>{" "}
+            </Link>
+
+            <Link href={"/product/wear-with-pride "}>
+              {" "}
+              <div className="lg:w-1/1 md:w-1/2 p-4 w-full  cursor-pointer  shodow-lg m-12">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh] block"
+                    src="https://source.unsplash.com/random/900×700/?formal-shoes"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Neptune
+                  </h2>
+                  <p className="mt-1">$12.00</p>
+                </div>
+              </div>{" "}
+            </Link>
+
+            <Link href={"/product/wear-with-pride "}>
+              <div className="lg:w-1/1 md:w-1/2 p-4 w-full cursor-pointer  shodow-lg m-12">
+                <a className="block relative  rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="m-auto md:mx-0 h-[30vh] block"
+                    src="https://source.unsplash.com/random/900×700/?formal-shoes"
+                  />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The 400 Blows
+                  </h2>
+                  <p className="mt-1">$18.40</p>
+                </div>
+              </div>{" "}
+            </Link>
+
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Formals;
