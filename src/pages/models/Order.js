c0066b6d4dee3ad0d8f6c5f670708edf32fd1183
roshan-cell <95081242+roshan-cell@users.noreply.github.com>
2023-05-15 23:50:06 +0530

    models

---
+++ src/pages/models/Order.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose') ;
+
+const Orderschema = new mongoose.Schema( {
+
+    userID : {type: String , require: true} ,
+    products : [{
+        productId : {type: String} ,
+        quantity : {type: Number , default: 1}   
+    }],
+    address : {type: String , require: true} ,
+    amount : {type: Number, require: true} ,
+    status : {type: String , default: "pending" , require: true}  ,
+} , {timestamps: true})
+
+
+export default mongoose.model("Order" , Orderschema) ;
